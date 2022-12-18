7a1417b18f2c87dd61f733b9109f8d45cf4269eb
a.kozlovska <a.kozlovska@intrum.com>
2022-12-18 20:33:05 +0200

    Lessons 98 - fix index page

--- src/test/java/guru/springframework/msscbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbrewery/web/controller/BeerControllerTest.java
@@ -61,7 +61,7 @@ class BeerControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.beerName").value("testBeerName"))
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-get",
                         pathParameters(
                             parameterWithName("beerId").description("UUID of desired beer to get")
                         ),
@@ -105,7 +105,7 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isCreated())
-                .andDo(document("v1/beer",
+                .andDo(document("v1/beer-new",
                     requestFields(
                         fields.withPath("id").ignored(),
                         fields.withPath("version").ignored(),
